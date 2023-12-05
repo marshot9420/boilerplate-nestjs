600a24951805de3315aee7cd074ad60c02e03edb
marshot <124557537+marshot9420@users.noreply.github.com>
2023-12-05 15:17:04 +0900

    [Configs] @nestjs/config + .env 파일 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { APP } from '@/constants';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || APP.NODE_ENV.DEVELOPMENT}`,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
