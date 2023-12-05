9aa8a309dd2065b5271e9e81e72f14633220e4ef
marshot <124557537+marshot9420@users.noreply.github.com>
2023-12-05 15:53:48 +0900

    [Feat] CORS 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { APP } from '@/constants';
+import { dbConfig } from '@/configs';
+import { APP, CONFIG } from '@/constants';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || APP.NODE_ENV.DEVELOPMENT}`,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get(CONFIG.DB),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
